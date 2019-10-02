23aa20ecb05a73a36cef3ceac32ac400869ccf4e
Victor <victor_polyukh@ukt.net>
2019-10-02 15:09:37 +0300

    Finished weather.py

--- weather.py
+++ weather.py
@@ -1,1 +1,14 @@
 import pyowm
+
+owm = pyowm.OWM('2dbd4d8ae880ca5737375946c337f14d', language="uk")
+
+place = input("Місто: ")
+
+observation = owm.weather_at_place(place)
+# observation = owm.weather_at_place('Kostopil,UA')
+w = observation.get_weather()
+
+temp = w.get_temperature('celsius')["temp"]
+
+print("В місті " + place + " зараз " + w.get_detailed_status())
+print("Температура: " + str(temp))
